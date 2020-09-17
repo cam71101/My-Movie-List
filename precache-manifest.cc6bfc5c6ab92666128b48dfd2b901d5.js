a89f7b005ef425db8777d6ec9be40fc4f1df6c84
cam71101 <david.fisher137@gmail.com>
2020-09-17 17:31:54 +0100

    Updates

---
+++ precache-manifest.cc6bfc5c6ab92666128b48dfd2b901d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9c36e7185d10c318d423ff07143b7d",
+    "url": "/The-Movie-List/index.html"
+  },
+  {
+    "revision": "228d04f1814ec3fbc598",
+    "url": "/The-Movie-List/static/css/main.37def858.chunk.css"
+  },
+  {
+    "revision": "ca987e4205e0a9723344",
+    "url": "/The-Movie-List/static/js/2.ee62eecf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/The-Movie-List/static/js/2.ee62eecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56cb345d4714031baadf",
+    "url": "/The-Movie-List/static/js/3.1c03eed5.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/The-Movie-List/static/js/3.1c03eed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fa718249eb04d23f6f",
+    "url": "/The-Movie-List/static/js/4.97f0d4a6.chunk.js"
+  },
+  {
+    "revision": "228d04f1814ec3fbc598",
+    "url": "/The-Movie-List/static/js/main.2f958b76.chunk.js"
+  },
+  {
+    "revision": "07b443efe0755cc43474",
+    "url": "/The-Movie-List/static/js/runtime-main.69d9854e.js"
+  }
+]);
