c8506a7904b871aa3bb38e987fd097f973d4c18e
cam71101 <david.fisher137@gmail.com>
2020-11-14 17:51:45 +0000

    Updates

---
+++ precache-manifest.74171acb56dd06075b4cda97a02ad2ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf4ad302c470735b1a83dbcd4795e71",
+    "url": "/My-Movie-List/index.html"
+  },
+  {
+    "revision": "90ed65a4b2642580b1fb",
+    "url": "/My-Movie-List/static/css/main.5204b28d.chunk.css"
+  },
+  {
+    "revision": "90ddfac7fa28200ea91c",
+    "url": "/My-Movie-List/static/js/2.c017e939.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/My-Movie-List/static/js/2.c017e939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627ad91f124a65594a23",
+    "url": "/My-Movie-List/static/js/3.c9f3538b.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/My-Movie-List/static/js/3.c9f3538b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2970b6c7d5912f4aea16",
+    "url": "/My-Movie-List/static/js/4.561dfb28.chunk.js"
+  },
+  {
+    "revision": "90ed65a4b2642580b1fb",
+    "url": "/My-Movie-List/static/js/main.60fb79f7.chunk.js"
+  },
+  {
+    "revision": "f149d9c8404983a19345",
+    "url": "/My-Movie-List/static/js/runtime-main.2083267e.js"
+  }
+]);
